90f38c17d4b427cd09a1a83bf7c39c13631cb600
ilsu-sunal <sunalilsu@gmail.com>
2024-07-02 11:50:04 +0300

    Main menu UI update
    - buttons added (start, about, settings, exit)
    - settings and exit panels created
    - all button scripts added

---
+++ U30-BioHarvest/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject exitConfirmationPanel;
+
+    //Bolum secme sahnesi - START butonu icin
+    public void LoadLevelSelection()
+    {
+        SceneManager.LoadScene("SpaceMissionMenu");
+    }
+
+    //Takim ve oyun hakkinda sahnesi - ABOUT butonu icin
+    public void LoadAboutTeam()
+    {
+        SceneManager.LoadScene("AboutTeamSecene");
+    }
+
+    //Ayarlar bolumu paneli - SETTINGS butonu icin
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    //Oyundan cikma onayi paneli - EXIT butonu icin
+    public void OpenExitConfirmation()
+    {
+        exitConfirmationPanel.SetActive(true);
+    }
+
+    public void CloseExitConfirmation()
+    {
+        exitConfirmationPanel.SetActive(false);
+    }
+
+    public void ConfirmExit()
+    {
+        Application.Quit();
+    }
+}
